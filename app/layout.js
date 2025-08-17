ee036eb323a8724d125f5fd14c98b5e9e091b726
Arya <aryazakaria67@gmail.com>
2025-08-17 09:39:48 +0700

    adding key gsc

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,10 @@ export const metadata = {
   description:
     "Website resmi Dusun Mendak, Kelurahan Tlogowatu, Kecamatan Kemalang, Kabupaten Klaten. Portal informasi dan pelayanan masyarakat.",
   keywords: "Dusun Mendak, Tlogowatu, Kemalang, Klaten, Jawa Tengah",
-  generator: 'v0.dev'
+  generator: 'v0.dev',
+  verification: {
+    google: "9W9Vm8UTgEiqT6aN0yTr4m_JhKoUPSk3yO_r9kJlU-8",
+  }
 }
 
 export default function RootLayout({ children }) {
